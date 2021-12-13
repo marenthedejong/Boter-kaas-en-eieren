3708dad4f34431c63d829a8fcb46f50b4a1c06ab
marenthedejong <57255299+marenthedejong@users.noreply.github.com>
2021-12-13 14:53:05 +0100

    Add files via upload

--- bekeee.py
+++ bekeee.py
@@ -1,2 +1,26 @@
-from bke import start
-start()
+from bke import MLAgent, is_winner,load, opponent,save, start,train
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
+    
+ 
+my_agent = MyAgent()
+
+train(my_agent, 3000)
+ 
+save(my_agent, 'MyAgent_3001')
+
+my_agent = load('MyAgent_3000')
+ 
+my_agent.learning = False
+ 
+start(player_x=my_agent)
+    
